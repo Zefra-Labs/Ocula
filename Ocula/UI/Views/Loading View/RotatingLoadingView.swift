7bf499b957adb162a508a1ec2536b3ae7c79970b
Tyson M <tysonjmiles@icloud.com>
2026-02-04 20:25:08 +1000

    Updates

---
+++ Ocula/UI/Views/Loading View/RotatingLoadingView.swift
@@ -0,0 +1,33 @@
+//
+//  RotatingLoadingView.swift
+//  Ocula
+//
+//  Created by Tyson Miles on 3/2/2026.
+//
+
+
+import SwiftUI
+
+struct RotatingLoadingView: View {
+    private let size: CGFloat = 50
+    private let duration: Double = 1.0
+
+    @State private var angle: Double = 0
+
+    var body: some View {
+        Image("loadingSymbol")
+            .resizable()
+            .renderingMode(.original)
+            .scaledToFit()
+            .frame(width: size, height: size)
+            .rotationEffect(.degrees(angle))
+            .task {
+                // Reset, then start a forever-rotation animation explicitly
+                angle = 0
+                withAnimation(.linear(duration: duration).repeatForever(autoreverses: false)) {
+                    angle = 360
+                }
+            }
+            .accessibilityLabel("Loading")
+    }
+}
